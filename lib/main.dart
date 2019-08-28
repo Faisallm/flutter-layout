72c9ac3a256b967e94e2d6894b719b8c5871f916
Faisallm <faisallawan1997@gmail.com>
2019-08-28 15:13:20 +0100

    flutter layout

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  Widget titleSection = Container(
+    padding: EdgeInsets.all(32.0),
+    child: Row(
+      children: <Widget> [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget> [
+              Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text('Oeschinen Lake Campground',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                )),
+              ),
+              Text('Kandersteg, Switzerland',
+              style: TextStyle(
+                color: Colors.grey[500],
+              )),
+            ],
+          ),
+        ),
+        Icon(Icons.star,
+        color: Colors.red),
+        Text('7'),  
+      ],
+    ),
+  );
+
+  Widget testSection = Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Text(
+       'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+    )
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget> [
+          _buildButtonColumn(color, Icons.call, "CALL"),
+          _buildButtonColumn(color, Icons.near_me, "ROUTE"),
+          _buildButtonColumn(color, Icons.share, "SHARE"),
+        ],
+      ),
+    );
+    return MaterialApp(
+      title: "Flutter layout demo",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter layout demo"),
+        ),
+        body: ListView(
+          children: <Widget> [
+            Image.asset(
+              'images/cat2.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            testSection,
+          ],
+        ),
+      ),
+    );
+  } 
+
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children : <Widget> [
+        Icon(icon,
+        color:color),
+        Container(
+          margin: const EdgeInsets.only(top:8.0),
+          child: Text(label,
+          style:TextStyle(
+            color:color,
+            fontSize: 12,
+            fontWeight: FontWeight.w200,
+          ))
+        ),   
+      ],
+    );
+  }
+}
